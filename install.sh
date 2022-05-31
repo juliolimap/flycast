fc35a1cce3518c6bbb465b810a0384a47537c4f4
juliolimap <81757489+juliolimap@users.noreply.github.com>
2022-05-31 09:12:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-sudo cp -r "~/flycast" "/usr/local/bin"
-sudo cp -r "~/flycast.png" "/usr/share/pixmaps"
-sudo cp -r "~/flycast.desktop" "/usr/share/applications"
+sudo pacman -Sy --noconfirm
+sudo pacman -S libzip --noconfirm
+sudo cp -r "flycast" "/usr/local/bin"
+sudo cp -r "flycast.png" "/usr/share/pixmaps"
+sudo cp -r "flycast.desktop" "/usr/share/applications"
 sudo chmod -R 777 /usr/local/bin/flycast
+
+flycast
